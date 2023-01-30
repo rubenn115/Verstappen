e5987ce700c9d2217fba547f991c3f3074661a4e
DESKTOP-SPTTQ5M\csi22 <csi22-rbermos@colegioaltair.net>
2023-01-30 09:44:49 +0100

    no message

---
+++ metodoCharAt/src/main/webapp/script/charAt.js
@@ -0,0 +1,17 @@
+function ejemplo(){
+	var frase = "Viva Red Bull";
+	document.write(frase+"<br>");
+	document.write("El carácter en el índice 0 es '" + frase.charAt(0) + "'"+"<br>")
+	document.write("El carácter en el índice 1 es '" + frase.charAt(1) + "'"+"<br>")
+	document.write("El carácter en el índice 2 es '" + frase.charAt(2) + "'"+"<br>")
+	document.write("El carácter en el índice 3 es '" + frase.charAt(3) + "'"+"<br>")
+	document.write("El carácter en el índice 4 es '" + frase.charAt(4) + "'"+"<br>")
+	document.write("El carácter en el índice 5 es '" + frase.charAt(5) + "'"+"<br>")
+	document.write("El carácter en el índice 6 es '" + frase.charAt(6) + "'"+"<br>")
+	document.write("El carácter en el índice 7 es '" + frase.charAt(7) + "'"+"<br>")
+	document.write("El carácter en el índice 8 es '" + frase.charAt(8) + "'"+"<br>")
+	document.write("El carácter en el índice 9 es '" + frase.charAt(9) + "'"+"<br>")
+	document.write("El carácter en el índice 10 es '" + frase.charAt(10) + "'"+"<br>")
+	document.write("El carácter en el índice 11 es '" + frase.charAt(11) + "'"+"<br>")
+	document.write("El carácter en el índice 12 es '" + frase.charAt(12) + "'")
+}
